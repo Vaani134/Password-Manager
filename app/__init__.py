f30bc88410fe86cb52663843673ff99d0dbf0993
lord-rahul <rahul717321+github@gmail.com>
2025-06-30 23:17:11 +0530

    :)

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,31 @@
-from flask import Flask , render_template
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
+#initoalizing db
+db=SQLAlchemy()
+
 def create_app():
+    #load env vars
     load_dotenv()
 
     app = Flask(__name__)
-
+    
+    #configd
+    app.config['SQLALCHEMY_DATABASE_URI']=os.getenv("DATABASE_URL")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+                
+    #intialize db with app
+    db.init_app(app)
+    
+    #route for frontend connection
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    #create database tables
+    with app.app_context():
+        db.create_all()
     
     return app
