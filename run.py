3b67aa9976596090fd7a5868f098f9f227b3be55
murali0631 <muralikurmapu1706@gmail.com>
2025-07-01 11:36:21 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from app import create_app
 import os
 
-app= create_app()
+app = create_app()
 
-if __name__=="__main__":
-    debug_mode =os.getenv('FLASK_ENV')
-    port= int(os.getenv('PORT'))
+if __name__ == "__main__":
+   
+    debug_env = os.getenv('FLASK_ENV', 'development')
+    debug_mode = debug_env == 'development' 
+
+    port = int(os.getenv('PORT', 5000)) 
 
     app.run(
         debug=debug_mode,
