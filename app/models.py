ad46352a172c4a641886e6dccefc382908db1537
Ashok kalluri <kalluriashok2627@gmail.com>
2025-07-25 15:42:13 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -15,6 +15,8 @@ class User(db.Model):
     Salted_masterkey=db.Column(db.String(50),nullable=False)
     create_time=db.Column(db.DateTime,default=datetime.utcnow)
     last_login=db.Column(db.DateTime)
+    name = db.Column(db.String(50), nullable=True)
+    photo_url = db.Column(db.String(200), nullable=True)
     #get all passwords fr the user
     passwords = db.relationship('User_passwords', backref='owner', lazy=True)
 
@@ -26,11 +28,20 @@ class User(db.Model):
 
 
     
+class Folder(db.Model):
+    __tablename__ = 'folders'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(20), db.ForeignKey('users.username'), nullable=False)
+    # Optionally, add created_at, updated_at fields
+
+# Update User_passwords to support folder association
 class User_passwords(db.Model):
     """user stored passwords model"""
     __tablename__='User_passwords'
     id=db.Column(db.Integer,primary_key=True)
     user_id=db.Column(db.Integer, db.ForeignKey('users.id'),nullable=False)
+    folder_id=db.Column(db.Integer, db.ForeignKey('folders.id'), nullable=True)
     encrypted_password = db.Column(db.Text, nullable=False)
     website_name=db.Column(db.String(100),nullable=False )
     website_url=db.Column(db.String(500),nullable=False )
@@ -38,6 +49,7 @@ class User_passwords(db.Model):
     notes=db.Column(db.String(500),nullable=False )
     create_time = db.Column(db.DateTime, default=datetime.utcnow)
     update_time = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    trashed = db.Column(db.Boolean, default=False)
     
     def encrypt_password(self,plain_password,master_password,user_salt):
         """Encrypt password using master passwrd and user salt"""
